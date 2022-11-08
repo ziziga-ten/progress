adaa412796c13426a794c868291b5be1e3388cbb
ziziga10 <113436414+ziziga10@users.noreply.github.com>
2022-11-08 12:10:47 +0100

    Create progress.js

---
+++ progress.js
@@ -0,0 +1,31 @@
+console.log("szeretnék megtanulni programozni");
+let a = "java script" 
+console.log(a);
+
+let b = 10
+let c = 12
+
+console.log(c*b);
+
+let i = 1
+while (i<11){
+    console.log(i +" " + "hazudni bűn");
+    i += 1;
+}
+
+
+for (let j = 10; j < 501; j += 2) {
+    console.log(j);
+}
+
+function sum(a, b){
+    
+}
+sum("guszti", )
+
+function myFunction(name) {
+    return "Hello " + name;
+   
+  }
+console.log(myFunction("guszti"))
+
